29f5065e9e6464b9b0e2b79402d198f033d0b282
pillypenkosa <pillypenkosa@gmail.com>
2023-04-07 18:49:45 +0300

    Buy DJS 2023-04-07 18:45

--- list/list.buy.need.js
+++ list/list.buy.need.js
@@ -4,21 +4,72 @@ const arrBuyNeed = [
 
 
 
+
+
+
+{
+	id: '2023_04_07_1848',
+	title: 'Наконечники для дротів // 3 - 4 мм // під обжимку',
+	store: { car: 1, autograd: 1, losk: 1, },
+},
 {
 	id: '2023_04_07_1733',
 	title: 'Олія моторна 5л // Aral Blue Tronic 10w40, ACEA A3/B4, API SN, VW 501.01/505.00',
-	store: { autograd: 1, losk: 1, },
+	store: { car: 1, autograd: 1, losk: 1, },
 },
 {
 	id: '2023_04_07_1732',
 	title: 'Разъем VAG // 3-контактный на катушку',
-	store: { autograd: 1, losk: 1, },
+	store: { car: 1, autograd: 1, losk: 1, },
 },
 {
 	id: '2023_04_07_1804',
 	title: 'Свічки запалювання // Denso К20ТТ',
-	store: { autograd: 1, losk: 1, },
+	store: { car: 1, autograd: 1, losk: 1, },
 },
+{
+	id: '2023_04_01_1200',
+	title: 'Пружини на зад // на універсал, а не седан',
+	store: { car: 1, autograd: 1, losk: 1, },
+},
+{
+	id: '2023_04_01_1201',
+	title: 'Спрей для очищення скла (оптичних поверхонь)',
+	store: { car: 1, autograd: 1, losk: 1, },
+},
+{
+	id: '2023_04_01_1202',
+	title: 'Подовжувач для насадок (штанга) // долізти в важкодоступні місця в авто',
+	store: { car: 1, autograd: 1, losk: 1, },
+},
+{
+	id: '2023_04_01_1203',
+	title: 'Ключ на 24 для керма // бажано трубка або головка',
+	store: { car: 1, autograd: 1, losk: 1, },
+},
+{
+	id: '2023_04_01_1204',
+	title: 'Рукоятка на перемикач КПП',
+	store: { car: 1, autograd: 1, losk: 1, },
+},
+{
+	id: '2023_04_01_1205',
+	title: 'Ручка на ручник // оригінал для B4',
+	store: { car: 1, autograd: 1, losk: 1, },
+},
+{
+	id: '2023_04_01_1206',
+	title: 'Роз\'єми пластикові // для з\'єднання дротів',
+	store: { car: 1, autograd: 1, losk: 1, },
+},
+
+{
+	id: '2023_04_01_1206',
+	title: 'Роз\'єм-розетка дверна для колонок // оригінал для B4',
+	store: { car: 1, autograd: 1, losk: 1, },
+},
+
+
 
 
 
@@ -42,8 +93,8 @@ const arrBuyNeed = [
 
 {
 	id: '2023_04_07_1730',
-	title: 'Безделушечка',
-	store: { atb: 1, },
+	title: 'Яблука',
+	store: { food: 1, atb: 1, },
 },
 
 
@@ -76,6 +127,12 @@ const arrStore = [
 	{ id: 'market' 			, title: 'Ц.Рынок'		, },
 	{ id: 'autograd' 		, title: 'Автоград' 	, },
 	{ id: 'losk' 			, title: 'Лоск' 		, },
+
+	{ id: 'food' 			, title: 'Продукти' 	, },
+	{ id: 'car' 			, title: 'VW Passat' 	, },
+	{ id: 'pc' 				, title: 'Компьютер' 	, },
+
+
 
 ];
 
