0ab7eb058d3f4aae3c4dd410f81ec3b980959e81
pillypenkosa <pillypenkosa@gmail.com>
2024-01-08 14:08:38 +0200

    Buy 2024-01-08 14:00

--- list/list.buy.need.js
+++ list/list.buy.need.js
@@ -3,48 +3,36 @@ const arrListBuyNeed = [
 
 
 
+
+
+{ title: 'Лоток для котів', hash: { household: 1, barabashovo: 1, market: 1, }, },
+{ title: 'Наповнювач для лотка', hash: { household: 1, barabashovo: 1, market: 1, }, },
+{ title: 'Папір А4, 1-2 пачки', hash: { household: 1, barabashovo: 1, market: 1, }, },
+
+
+
+{ title: 'Шприц великий для промивки', hash: { household: 1, barabashovo: 1, market: 1, }, },
+
 { title: 'Сушилки для взуття', hash: { household: 1, barabashovo: 1, market: 1, }, },
 { title: 'Рюкзак', hash: { barabashovo: 1, market: 1, }, },
-//{ title: 'Пульт GiOne S1016', hash: { barabashovo: 1, market: 1, }, },
+{ title: 'Ложка металева', hash: { household: 1, barabashovo: 1, market: 1, }, },
 
 { title: 'Парасолька 1.5-місна', hash: { household: 1, barabashovo: 1, market: 1, }, },
 
-//{ title: 'Шурупчик для стільця 3.6 х 24', hash: { household: 1, barabashovo: 1, market: 1, }, },
-//{ title: 'Зарядку для мобіли з кабелеми', hash: { household: 1, barabashovo: 1, market: 1, }, },
-
 { title: 'Перчатки безпалі', hash: { household: 1, barabashovo: 1, market: 1, }, },
-//{ title: 'Жезл', hash: { household: 1, barabashovo: 1, market: 1, }, },
 { title: 'Кроси-черевики без хутра', hash: { household: 1, barabashovo: 1, market: 1, }, },
-
-//{ title: 'Чашка велика', hash: { household: 1, barabashovo: 1, market: 1, }, },
-
-
-
-//{ title: 'Ложки одноразові', hash: { household: 1, barabashovo: 1, market: 1, }, },
-//{ title: 'Тарілки одноразові', hash: { household: 1, barabashovo: 1, market: 1, }, },
-//{ title: 'Ніж зубастий', hash: { household: 1, barabashovo: 1, market: 1, }, },
-
-//{ title: 'Зубна паста', hash: { household: 1, barabashovo: 1, market: 1, }, },
-//{ title: 'Зубна щітка', hash: { household: 1, barabashovo: 1, market: 1, }, },
-//{ title: 'Палички вушні', hash: { household: 1, barabashovo: 1, market: 1, }, },
-//{ title: 'Шприц великий для промивки', hash: { household: 1, barabashovo: 1, market: 1, }, },
-//{ title: 'Паста Теймурова', hash: { household: 1, barabashovo: 1, market: 1, }, },
-
-//{ title: 'Сумка велика господарча', hash: { household: 1, barabashovo: 1, market: 1, }, },
-//{ title: 'Ганчірка для взуття', hash: { household: 1, barabashovo: 1, market: 1, }, },
-//{ title: 'Щітка для взуття', hash: { household: 1, barabashovo: 1, market: 1, }, },
-//{ title: 'Ганчірка для пилу', hash: { household: 1, barabashovo: 1, market: 1, }, },
-//{ title: 'Тремпеля', hash: { household: 1, barabashovo: 1, market: 1, }, },
+{ title: 'Кроси або ботинки під дощ', hash: { barabashovo: 1, market: 1, }, },
+{ title: 'Подштанники 2-3 пари', hash: { barabashovo: 1, market: 1, }, },
 
 
 
 
 
-{ title: 'Труби для димоходу', hash: { household: 1, barabashovo: 1, market: 1, }, },
+{ title: 'Труби для димоходу, 130 мм', hash: { household: 1, barabashovo: 1, market: 1, }, },
 
 { title: 'Контейнер для печива )))))))))', hash: { car: 1, losk: 1, autograd: 1, }, },
 //{ title: 'Карабінчики пластикові на бронік', hash: { market: 1, }, },
-{ title: 'Вимикач світла для діодних ламп в багажнику', hash: { household: 1, barabashovo: 1, market: 1, }, },
+{ title: 'Вимикач світла для діодних ламп в багажнику', hash: { car: 1, barabashovo: 1, market: 1, }, },
 { title: 'Вимикач світла для гаража', hash: { household: 1, barabashovo: 1, market: 1, }, },
 
 //{ title: 'Олія моторна 5л <br/> Aral Blue Tronic 10w40, ACEA A3/B4, API SN, VW 501.01/505.00', hash: { car: 1, autograd: 1, losk: 1, }, },
@@ -53,29 +41,12 @@ const arrListBuyNeed = [
 { title: 'Фіксатор-упор переднього сидіння ( замість скоби )', hash: { car: 1, losk: 1, autograd: 1, }, },
 
 
-//{ id: '2023_08_14_1201', title: 'Паста Теймурова', hash: { pharmacy: 1, }, },
-//{ id: '2023_10_18_1600', title: 'Батарейки для ліхтарика ААА - 3 шт', hash: { atb: 1, rost: 1, barabashovo: 1, market: 1, }, },
+//{ title: 'Паста Теймурова', hash: { pharmacy: 1, }, },
 
-//{ id: '2023_10_12_1000', title: 'Лампи 4000К для гаража 2-3 шт', hash: { atb: 1, household: 1, barabashovo: 1, market: 1, }, },
 { title: 'Жгут до замка запалювання з цілим чорно-жовтим IGN-15', hash: { car: 1, losk: 1, autograd: 1, barabashovo: 1, market: 1, }, },
 
 
-
-
-//{ id: '2023_09_19_1851', title: 'Батарейки 2025 2 шт', hash: { losk: 1, autograd: 1, barabashovo: 1, market: 1, }, },
-//{ id: '2023_09_19_1850', title: 'Запобіжник 25А з колодкою-тримачем', hash: { losk: 1, autograd: 1, barabashovo: 1, market: 1, }, },
-//{ id: '2023_09_18_1905', title: 'Ліхтарик', hash: { losk: 1, autograd: 1, barabashovo: 1, market: 1, }, },
-//{ id: '2023_09_18_1901', title: 'Клеми-кільця під болт 6мм, для кріпера під дроти до 1мм<sup>2</sup>', hash: { car: 1, losk: 1, autograd: 1, barabashovo: 1, market: 1, }, },
-//{ id: '2023_09_18_1900', title: 'Двосторонній скотч для антени та датчику удара', hash: { car: 1, losk: 1, autograd: 1, barabashovo: 1, market: 1, }, },
-//{ id: '2023_09_17_2000', title: 'Запобіжник 25 А з колодкою для сигналізації', hash: { car: 1, losk: 1, autograd: 1, barabashovo: 1, market: 1, }, },
-
-{ title: 'Подштанники 2-3 пари', hash: { barabashovo: 1, market: 1, }, },
-{ title: 'Джинси', hash: { barabashovo: 1, market: 1, }, },
-{ title: 'Кроссовки или ботинки под дождь', hash: { barabashovo: 1, market: 1, }, },
-
-
 { title: 'Неонова лампа 40-60 В', hash: { car: 1, losk: 1, autograd: 1, barabashovo: 1, market: 1, }, },
-//{ id: '2023_09_14_1152', title: 'Реле. Лоск,  ряд 20, місце 33-35  (ФОП Пелих П.В.)', hash: { car: 1, losk: 1, autograd: 1, barabashovo: 1, market: 1, }, },
 { title: 'Діоди 1N4007 або FR104 (для реле розвантаження)', hash: { car: 1, losk: 1, autograd: 1, barabashovo: 1, market: 1, }, },
 { title: 'Фари тюнінговані LPVW97', hash: { car: 1, losk: 1, autograd: 1, barabashovo: 1, market: 1, }, },
 
@@ -89,25 +60,23 @@ const arrListBuyNeed = [
 { title: 'Болти на 6 з лівою різьбою <br/>на ручки дверей', hash: { car: 1, autograd: 1, losk: 1, barabashovo: 1, market: 1, }, },
 
 
-//{ id: '2023_07_30_0931', title: 'Риб\'ячий жир', hash: { atb: 1, }, },
-//{ id: '2023_07_30_0930', title: 'Кальцій', hash: { atb: 1, }, },
+//{ title: 'Риб\'ячий жир', hash: { atb: 1, }, },
+//{ title: 'Кальцій', hash: { atb: 1, }, },
 
 { title: 'Графіт без меді', hash: { car: 1, autograd: 1, losk: 1, barabashovo: 1, market: 1, }, },
 { title: 'Паста Гої', hash: { car: 1, autograd: 1, losk: 1, barabashovo: 1, market: 1, }, },
-//{ id: '2023_06_08_2205', title: 'Таблетки від головного болю', hash: { pharmacy: 1, }, },
-//{ id: '2023_06_05_1900', title: 'Пушсало', hash: { car: 1, autograd: 1, losk: 1, barabashovo: 1, market: 1, }, },
+
 { title: 'Ручка в набір головок ( яка загубилась )', hash: { tool: 1, car: 1, autograd: 1, losk: 1, barabashovo: 1, market: 1, }, },
 { title: 'Диски R15 та шини літні', hash: { car: 1, autograd: 1, losk: 1, barabashovo: 1, market: 1, }, },
 { title: 'Коротка трубка клапана хх, яка йде в впускний колектор', hash: { car: 1, autograd: 1, losk: 1, }, },
 { title: 'Кінцевики-кнопки відкриття капоту та ляди для сигналізації', hash: { car: 1, autograd: 1, losk: 1, }, },
-//{ id: '2023_04_07_1910', title: 'Піни та резинові втулки для роз\'ємів', hash: { car: 1, autograd: 1, losk: 1, }, },
-//{ id: '2023_04_07_1804', title: 'Свічки запалювання <br/>Denso К20ТТ', hash: { car: 1, autograd: 1, losk: 1, }, },
-{ title: 'Пружини на зад <br/>на універсал, а НЕ седан', hash: { car: 1, autograd: 1, losk: 1, }, },
+
+//{ title: 'Свічки запалювання <br/>Denso К20ТТ', hash: { car: 1, autograd: 1, losk: 1, }, },
 { title: 'Ключ на 24 для керма <br/>трубка або головка', hash: { tool: 1, car: 1, autograd: 1, losk: 1, barabashovo: 1, market: 1, }, },
 { title: 'Рукоятка на перемикач КПП', hash: { car: 1, autograd: 1, losk: 1, }, },
-//{ id: '2023_04_01_1205', title: 'Ручка на ручник <br/>оригінал для B4', hash: { car: 1, autograd: 1, losk: 1, }, },
-//{ id: '2023_04_01_1206', title: 'Роз\'єми пластикові <br/>для з\'єднання дротів', hash: { car: 1, autograd: 1, losk: 1, }, },
-//{ id: '2023_04_01_1206', title: 'Роз\'єм-розетка дверна для колонок <br/>оригінал для B4', hash: { car: 1, autograd: 1, losk: 1, }, },
+
+
+
 { title: 'Килимки на підлогу', hash: { car: 1, autograd: 1, losk: 1, }, },
 { title: 'Телевізор на кухню', hash: { home: 1, }, },
 { title: 'Замок кодовий на хвіртку', hash: { household: 1, barabashovo: 1, market: 1, }, },
@@ -116,7 +85,7 @@ const arrListBuyNeed = [
 { title: 'Вітаміни для очей <br/>Чорниця', hash: { pharmacy: 1, }, },
 
 
-//{ id: '2023_06_03_1644', title: 'Ополаскувач рта', hash: { atb: 1, }, },
+//{ title: 'Ополаскувач рта', hash: { atb: 1, }, },
 
 
 
